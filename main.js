bd075d59aab36cf87f1aab5e1ed9241f03e7ef31
EtimGeorge <saencrystal@gmail.com>
2024-06-14 22:20:44 +0100

    14/06/24 commit

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const hamburger = document.querySelector('.hamburger');
-//     const menu = document.querySelector('.menu-option');
-
-//     hamburger.addEventListener('click', () => {
-//       menu.classList.toggle('show');
-//     });
-//   });
-
+// ========THIS METHOD IS USED WHEN THE HAMBURGER LINES ARE CREATED RATHER THAN USING ICONS FOR THE OPEN AND CLOSE TOGGLE.=======
 
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
@@ -18,3 +10,37 @@ hamburgerMenu.addEventListener('click', () => {
     line.classList.toggle('open');
   });
 });
+
+
+
+// =========THIS METHOD IS USED WHEN THE HAMBURGER LINES ARE NOT CREATED RATHER ICONS WAS USED FOR THE OPEN AND CLOSE TOGGLE.=======
+
+
+// const  mobile_menu = document.querySelector('.nav_mobile');
+// const  fa_open = document.getElementById('fa-open');
+// const  fa_close = document.getElementById('fa-close');
+
+// fa_open.addEventListener('click',
+//     function(){
+//         if(mobile_menu.style.display === 'none'){
+//             mobile_menu.style.display = 'block';
+//             fa_open.style.display = 'none';
+//             fa_close.style.display = 'block';
+//         } else {
+//             mobile_menu.style.display = 'none';
+//             fa_open.style.display = 'block';
+//             fa_close.style.display = 'none';
+//         }
+// });
+
+// fa_close.addEventListener('click',
+//     function(){
+//         if(mobile_menu.style.display === 'block'){
+//             mobile_menu.style.display = 'none';
+//             fa_open.style.display = 'block';
+//             fa_close.style.display = 'none';
+//         }
+// });
+
+
+
